655c82cbba830a9116ea6a85e03bc78e2e5ad06a
TaisiaKol <tasia.pcma.le4@mail.ru>
2022-06-22 14:38:13 +0300

    init miles

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int prise = 100;
+        int one_mile = 20;
+        int mile = prise / one_mile;
+        System.out.println(mile);
+    }
+}
+}
