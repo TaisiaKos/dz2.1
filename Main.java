1fec2b2df9426dde79b15c7af0d1f49d0ca8fb05
TaisiaKol <tasia.pcma.le4@mail.ru>
2022-06-22 14:58:52 +0300

    transfer main

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int prise = 100;
-        int one_mile = 20;
-        int mile = prise / one_mile;
-        System.out.println(mile);
-    }
-}
-}
